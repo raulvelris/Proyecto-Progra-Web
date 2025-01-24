ba00d0b7f28f4f1fa0e6b70db487e55052825e71
esteban_terrones <20223007@aloe.ulima.edu.pe>
2025-01-23 22:39:55 -0500

    avance login esteban

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// import './index.css'
+import Login from './Login'
 // import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Login/>
   </StrictMode>,
 )
