f8cb8065a84cdeeae9b738854f7a2bb3d30a797b
Erling2 <20223058@aloe.ulima.edu.pe>
2025-01-25 18:55:35 -0500

    Subiendo mi branch

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-// import './index.css'
-import Login from './Login'
-// import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import RecuperarContraseña from './pages/RecuperarContraseña';
+//import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/recuperar-contraseña" element={<RecuperarContraseña />} />
+      </Routes>
+    </Router>
+  );
+};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Login/>
+    <App />
   </StrictMode>,
-)
+);
---
+++ src/pages/EgresosxFecha.tsx
@@ -0,0 +1,10 @@
+//import React from "react";
+import '../TailWind.css';
+
+const EgresosxFecha = () => {
+    return <div>
+        hola
+    </div>
+}
+
+export default EgresosxFecha
