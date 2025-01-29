3d77d7f07cfab7613c9b1ad5f3f82b37a323ec2a
Erling2 <20223058@aloe.ulima.edu.pe>
2025-01-28 23:59:49 -0500

    Cambios en RecuperarContraseña

--- src/pages/RecuperarContraseña.tsx
+++ src/pages/RecuperarContraseña.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import '../TailWind.css';
+import '../tailwind.css';
 
 const RecuperarContraseña: React.FC = () => {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-200">
       <div className="flex flex-col items-center border-none rounded-xl bg-white p-10 pb-5">
-        <h1 className="text-3xl font-bold text-gray-700 mb-10">Recuperar contraseña</h1>
-        <input className="border border-gray-400 rounded px-4 py-2 w-64 w-84 my-2" type="text" placeholder="Ingresar correo"/>
-        <button className="bg-blue-500 text-white px-4 py-2 rounded w-84 mt-2 hover:bg-blue-600 active:bg-blue-700 cursor-pointer transition duration-200" type="button">Recibir código de verificación</button>
+        <h1 className="text-3xl font-bold text-gray-700 mb-10">Nueva contraseña</h1>
+        <input className="border border-gray-400 rounded px-4 py-2 w-64 w-84 my-2" type="text" placeholder="Nueva contraseña"/>
+        <input className="border border-gray-400 rounded px-4 py-2 w-64 w-84 my-2" type="text" placeholder="Re-escribir nueva contraseña"/>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded w-84 mt-7 hover:bg-blue-600 active:bg-blue-700 cursor-pointer transition duration-200" type="button">Aceptar</button>
       </div>
     </div>
   );
