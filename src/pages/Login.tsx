dfc46143a350d63f118b5384c7676c655bd37431
esteban_terrones <20223007@aloe.ulima.edu.pe>
2025-01-31 09:38:12 -0500

    redireccion a /appadmin/

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../tailwind.css';
 
@@ -15,15 +15,33 @@ const Login: React.FC = () => {
   };
 
   const handleLogin = () => {
-    navigate('/app/dashboard');
+    console.log(email, password);
+    if (email === "admin@" && password === "admin") {
+      navigate('/appadmin/dashboard');
+    } else {
+      navigate('/app/dashboard');
+    }
   };
+
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const emailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setEmail(event.target.value);
+      // console.log(event.target.value);
+  }
+
+  const passwordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setPassword(event.target.value);
+      // console.log(event.target.value);
+  }
 
   return (
     <div className="flex justify-center items-center h-screen bg-gray-200">
       <div className="flex flex-col items-center border-none rounded-xl bg-white p-10 pb-5">
         <h1 className="text-3xl font-bold text-gray-700 mb-10">Log In</h1>
-        <input className="border border-gray-400 rounded px-4 py-2 w-64 w-84 my-2" type="text" placeholder="Ingresar correo"/>
-        <input className="border border-gray-400 rounded px-4 py-2 w-64 w-84 my-2" type="password" placeholder="Ingresar contraseña"/>
+        <input className="border border-gray-400 rounded px-4 py-2 w-84 my-2" type="text" placeholder="Ingresar correo" value={email} onChange={emailChange}/>
+        <input className="border border-gray-400 rounded px-4 py-2 w-84 my-2" type="password" placeholder="Ingresar contraseña" value={password} onChange={passwordChange}/>
         <a className="text-blue-400 underline mb-4 cursor-pointer" onClick={handleForgotPassword}>¿Olvidaste tu contraseña?</a>
         <button className="bg-blue-500 text-white px-4 py-2 rounded w-84 mt-2 hover:bg-blue-600 active:bg-blue-700 cursor-pointer transition duration-200" type="button" onClick={handleLogin}>Ingresar</button>
         <p className="text-gray-500 p-1">O</p>
