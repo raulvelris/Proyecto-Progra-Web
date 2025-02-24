6a7bd45bf5534da9932805e9582cce38726f39f5
Erling2 <20223058@aloe.ulima.edu.pe>
2025-02-23 19:22:02 -0500

    Modificando RecuperarContraseña

--- src/pages/RecuperarContraseña.tsx
+++ src/pages/RecuperarContraseña.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+//import React from 'react';
 import '../tailwind.css';
 import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
 
-const RecuperarContraseña: React.FC = () => {
+const RecuperarContraseña = () => {
 
   const navigate = useNavigate();
   
@@ -14,9 +15,18 @@ const RecuperarContraseña: React.FC = () => {
     <div className="flex justify-center items-center h-screen bg-gray-200">
       <div className="flex flex-col items-center border-none rounded-xl bg-white p-10 pb-5">
         <h1 className="text-3xl font-bold text-gray-700 mb-10">Nueva contraseña</h1>
-        <input className="border border-gray-400 rounded px-4 py-2 w-64 w-84 my-2" type="text" placeholder="Nueva contraseña"/>
-        <input className="border border-gray-400 rounded px-4 py-2 w-64 w-84 my-2" type="text" placeholder="Re-escribir nueva contraseña"/>
-        <button className="bg-blue-500 text-white px-4 py-2 rounded w-84 mt-7 hover:bg-blue-600 active:bg-blue-700 cursor-pointer transition duration-200" type="button" onClick={redirectClick}>Aceptar</button>
+        <input className="border border-gray-400 rounded px-4 py-2 w-64 my-2" 
+              type="text" 
+              placeholder="Ingresar correo"/>
+        <input className="border border-gray-400 rounded px-4 py-2 w-64 my-2" 
+              type="password"
+              placeholder="Nueva contraseña"/>
+        <input className="border border-gray-400 rounded px-4 py-2 w-64 my-2" 
+                type="password" 
+                placeholder="Re-escribir nueva contraseña"/>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded w-64 mt-7 hover:bg-blue-600 active:bg-blue-700 cursor-pointer transition duration-200"
+        type="button"
+        onClick={redirectClick}>Aceptar</button>
       </div>
     </div>
   );
