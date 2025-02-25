af358240d8a2bc911505e28ed7fbe86444db06ed
esteban_terrones <esteban20001118@gmail.com>
2025-02-25 13:29:42 -0500

    microcambios fabricio

--- src/services/AccessLogService.ts
+++ src/services/AccessLogService.ts
@@ -1,13 +1,28 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/admin";
+// const API_URL = "http://localhost:5000/accesslogs";
+
+// export const getAccessLogs = async () => {
+//     try {
+//         const response = await axios.get(`${API_URL}/accesslogs`);
+//         return response.data;
+//     } catch (error) {
+//         console.error("Error fetching access logs:", error);
+//         return null;
+//     }
+// };
 
 export const getAccessLogs = async () => {
+    let res: any;
     try {
-        const response = await axios.get(`${API_URL}/accesslogs`);
-        return response.data;
-    } catch (error) {
-        console.error("Error fetching access logs:", error);
-        return null;
+        res = await fetch("http://localhost:5000/accesslogs"); 
+    } catch (e) {
+        console.log(e);
     }
+
+    if (!res.ok) {
+        res.json({ msg: "Error fetching access logs" });
+    }
+
+    return await res.json();
 };
