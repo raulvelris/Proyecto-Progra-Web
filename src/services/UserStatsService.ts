bae82a7df6790cc4526e8477f340ceb06052696b
fabricio <20224387@aloe.ulima.edu.pe>
2025-02-25 12:32:47 -0500

    funciona por fvor

---
+++ src/services/UserStatsService.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+
+const API_URL = "http://localhost:5000/api/admin";
+
+
+export const getUserStatistics = async () => {
+    try {
+        const response = await axios.get(`${API_URL}/user-statistics`);
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching user statistics:", error);
+        return null;
+    }
+};
